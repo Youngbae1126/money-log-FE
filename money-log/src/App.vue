bb6cff39337824d0eee106c18cb1550cfb164523
ParkJungYeol <trg22@naver.com>
2025-04-08 11:50:55 +0900

    feat: 거래내역 추가 플로팅 버튼, 거래내역 추가 모달 창 구현

--- money-log/src/App.vue
+++ money-log/src/App.vue
@@ -1,9 +1,43 @@
 <script setup>
+import { ref } from 'vue'
 import { RouterView } from 'vue-router'
+import MoneyInputModal from './components/MoneyInputModal.vue'
+import FloatingButton from './components/FloatingButton.vue'
+
+const isModalOpen = ref(false)
+
+const openModal = () => {
+  isModalOpen.value = true
+}
+
+const closeModal = () => {
+  isModalOpen.value = false
+}
+
+const handleSubmit = formData => {
+  console.log('제출된 데이터:', formData)
+  // TODO: 여기에서 데이터를 저장하거나 처리하는 로직을 추가하세요
+}
 </script>
 
 <template>
-  <RouterView />
+  <div class="app-container">
+    <RouterView />
+    <FloatingButton :onClick="openModal" />
+    <MoneyInputModal
+      :isOpen="isModalOpen"
+      @close="closeModal"
+      @submit="handleSubmit"
+    />
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.app-container {
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 2rem;
+  position: relative;
+  min-height: 100vh;
+}
+</style>
