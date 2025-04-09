e558de3c5f3470cf28fc978cad1b15f483ee970a
Youngbae1126 <devhyun1126@gmail.com>
2025-04-09 11:21:18 +0900

    :bug: FIX: 대시보드 에러 해결 - API URL 주소 잘못되어 있어서 변경 - 목표, 현재 금액을 goalStore, transactionStore 이용해서 로직 수정

--- money-log/src/components/GoalTracker.vue
+++ money-log/src/components/GoalTracker.vue
@@ -1,45 +1,53 @@
 <script setup>
 import { ref, computed, onMounted } from 'vue'
-import axios from 'axios'
+// import axios from 'axios'
+import { useGoalStore } from '@/stores/goal'
+import { useTransactionStore } from '@/stores/transactionStore'
+
+const goalStore = useGoalStore()
+const transactionStore = useTransactionStore()
 
 // 날짜 → 'YYYY-MM'
-const now = new Date()
-const currentMonth = `${now.getFullYear()}-${String(now.getMonth() + 1).padStart(2, '0')}`
+// const now = new Date()
+// const currentMonth = `${now.getFullYear()}-${String(now.getMonth() + 1).padStart(2, '0')}`
 
-// 상태값
-const goalAmount = ref(0)
-const currentSpending = ref(0)
+// goalStore 내에 정의된 목표값을 가져옴
+const goalAmount = ref(goalStore.targetExpense)
 
 // 지출 비율 계산
 const percentageUsed = computed(() => {
   if (goalAmount.value === 0) return 0
-  const percent = (currentSpending.value / goalAmount.value) * 100
+  const percent = (transactionStore.totalExpense / goalAmount.value) * 100
   return percent > 100 ? 100 : Math.floor(percent)
 })
 
 // 데이터 로딩
 onMounted(() => {
-  axios
-    .get('http://localhost:3001/goal')
-    .then(response => {
-      const thisMonthGoal = response.data.find(
-        goal => goal.month === currentMonth,
-      )
-      if (thisMonthGoal) {
-        goalAmount.value = thisMonthGoal.targetExpense
-        currentSpending.value = thisMonthGoal.currentExpense
-      }
-    })
-    .catch(error => {
-      console.error('데이터 불러오기 실패:', error)
-    })
+  // axios
+  //   .get('http://localhost:5500/goal')
+  //   .then(response => {
+  //     // const thisMonthGoal = response.data
+  //     // thisMonthGoal.find(goal => goal.month === currentMonth)
+  //     const thisMonthGoal = response.data.month
+  //     if (thisMonthGoal) {
+  //       goalAmount.value = goalStore.targetExpense
+  //       currentSpending.value = goalStore.currentExpense
+  //     }
+  //   })
+  //   .catch(error => {
+  //     console.error('데이터 불러오기 실패:', error)
+  //   })
+
+  // 위에 있는 코드를 pinia의 store 활용
+  goalStore.getGoalInfo()
+  transactionStore.getTransactionInfo()
 })
 </script>
 
 <template>
   <div class="GoalTracker">
-    <p>목표 금액: {{ goalAmount.toLocaleString() }}원</p>
-    <p>현재 지출: {{ currentSpending.toLocaleString() }}원</p>
+    <p>목표 금액: {{ goalStore.targetExpense.toLocaleString() }}원</p>
+    <p>현재 지출: {{ transactionStore.totalExpense.toLocaleString() }}원</p>
     <p>사용 비율: {{ percentageUsed }}%</p>
   </div>
 </template>
