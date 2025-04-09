574f2e63784bb6a8fe4e7e0d690e9c8277dd9fea
syMoon7 <tldbs55123@gmail.com>
2025-04-10 03:36:32 +0900

    Revert "Refactor/#3 detail"

--- money-log/src/components/Header.vue
+++ money-log/src/components/Header.vue
@@ -1,65 +1,11 @@
 <!-- eslint-disable vue/multi-word-component-names -->
-<script setup>
-import { onMounted } from 'vue'
-import { useUserStore } from '../stores/users'
-
-const userStore = useUserStore()
-
-onMounted(() => {
-  userStore.getUserInfo()
-})
-</script>
+<script setup></script>
 
 <template>
-  <header>
-    <div class="header">
-      <RouterLink to="/mypage">
-        <div class="nickname-container">
-          <img
-            src="../assets/profile-icon.svg"
-            alt="프로필 아이콘"
-            class="profile__icon"
-          />
-          <span class="nickname__text">{{ userStore.nickname }}</span> 님!
-        </div>
-      </RouterLink>
-      <h1>
-        <RouterLink to="/"
-          ><img src="../assets/logo.svg" alt="로고"
-        /></RouterLink>
-      </h1>
-    </div>
-  </header>
+  <div class="Header"></div>
 </template>
 
 <style scoped>
-.header {
-  padding: 0.8rem;
-  background-color: transparent;
-}
-.header {
-  max-width: 1920px;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  width: calc(100% - 2rem);
-  margin: auto;
-}
-.nickname-container {
-  font-family: 'Pretendard-Regular';
-  display: flex;
-  align-items: center;
-  text-align: center;
-  gap: 0.3rem;
-  font-size: 1.3rem;
-}
-.profile__icon {
-  width: 40px;
-  height: 40px;
-}
-.nickname__text {
-  color: var(--main-color);
-  font-weight: bold;
-  font-size: 1.35rem;
+.Header {
 }
 </style>
--- money-log/src/router/index.js
+++ money-log/src/router/index.js
@@ -1,39 +1,6 @@
-import DashBoard from '@/views/DashBoard.vue'
-import Layout from '@/views/Layout.vue'
-import Mypage from '@/views/Mypage.vue'
-import TransactionDetail from '@/views/TransactionDetail.vue'
-import TransactionList from '@/views/TransactionList.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
-const routes = [
-  {
-    path: '/',
-    name: 'root',
-    component: Layout,
-    children: [
-      {
-        path: '/',
-        name: 'main',
-        component: DashBoard,
-      },
-      {
-        path: '/list',
-        name: 'transaction-list',
-        component: TransactionList,
-      },
-      {
-        path: '/detail',
-        name: 'transaction-detail',
-        component: TransactionDetail,
-      },
-    ],
-  },
-  {
-    path: '/mypage',
-    name: 'mypage',
-    component: Mypage,
-  },
-]
+const routes = []
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
