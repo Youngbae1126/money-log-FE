b9f5ea5e92c3415d42723aca0a68793c7cccb576
Youngbae1126 <devhyun1126@gmail.com>
2025-04-11 04:46:47 +0900

    :sparkles: FEAT: 대시보드에서 캘린더 클릭 시 리스트 페이지로 이동 구현

--- money-log/src/views/DashBoard.vue
+++ money-log/src/views/DashBoard.vue
@@ -14,7 +14,9 @@ import MostSpent from '@/components/MostSpent.vue'
     </div>
     <div class="dashboard__right-container">
       <IEComparing />
-      <Calender />
+      <RouterLink to="/list">
+        <Calender />
+      </RouterLink>
     </div>
   </div>
 </template>
@@ -24,16 +26,11 @@ import MostSpent from '@/components/MostSpent.vue'
   display: flex;
   flex-wrap: wrap;
   gap: 5rem;
-  /* width: calc(100% - 2rem); */
   max-width: 1920px;
-  /* margin: 2rem auto; */
-  /* margin-right: 2rem; */
   padding: 1rem 2rem;
   box-sizing: border-box;
-  /* justify-content: space-between; */
 }
 .dashboard__left-container {
-  /* flex-grow: 0.1; */
   flex: 0 0 20%;
   display: flex;
   flex-direction: column;
@@ -41,7 +38,6 @@ import MostSpent from '@/components/MostSpent.vue'
   margin-left: 1rem;
 }
 .dashboard__right-container {
-  /* flex-grow: 0.9; */
   flex: 0 0 70%;
   display: flex;
   flex-direction: column;
